916f72101a283e5aa44a2be1f0cfc2fd8e1b7faa
Leonardo Rodrigues <leo-feitosa@live.com>
2022-03-19 17:00:29 -0300

    parte um da criação da biblioteca

---
+++ Bytbank.Modelos/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Bytbank.Modelos;
+public static class Class1
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
